445eebf0a74a84737baeb643b691cd18ee7ef023
timf34 <farrelti@tcd.ie>
2022-06-24 23:19:49 +0100

    added `get_writer`

--- opencv_utils.py
+++ opencv_utils.py
@@ -1,12 +1,21 @@
 import cv2
 import numpy as np
 import os
+from typing import Tuple
+
 
 def get_cam() -> cv2.VideoCapture:
     """
         This function returns the cv2.VideoCapture object
     """
     return cv2.VideoCapture(0)
+
+
+def get_writer(frame_size: Tuple[int, int], file_name: str = 'test.avi', fps: int = 10) -> cv2.VideoWriter:
+    """
+        This function returns the cv2.VideoWriter object
+    """
+    return cv2.VideoWriter(file_name, cv2.VideoWriter_fourcc(*'XVID'), fps, frame_size)
 
 
 def get_frame(cam: cv2.VideoCapture) -> np.ndarray:
@@ -34,3 +43,4 @@ def create_directory(directory_name: str) -> None:
         print('Directory created successfully')
     else:
         print('Directory already exists')
+
